deaccd05722d16941b54247947d2ffff8b6e0fe6
Rachit3784 <grachit736@gmail.com>
2026-02-05 16:45:34 +0530

    X6

--- utils/OtpMailer.js
+++ utils/OtpMailer.js
@@ -1,43 +1,41 @@
 import nodemailer from 'nodemailer'
-import { Email, NODEMAILER_API_KEY } from '../config/ENV_variable.js'
-
-
-
-
-if (!NODEMAILER_API_KEY) {
-    console.error('ERROR: SENDGRID_API_KEY is not set in environment variables!');
-}
+import { Email, Email_Pass } from '../config/ENV_variable.js'
 
 const transport = nodemailer.createTransport({
-    host: 'smtp.sendgrid.net',
-    port: 587,
-    secure: false,
-    auth: {
-        user: Email,
-        pass: NODEMAILER_API_KEY
-    },
-    connectionTimeout: 30000,
-    socketTimeout: 30000
+      host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+    auth : {
+        user : Email,
+        pass : Email_Pass
+    }
 });
 
-export const SendOtpToUser = async (data) => {
-    const mailOptions = {
-        from: Email,
-        to: data.userEmail,
-        subject: 'OTP To Register As Admin',
-        text: data.otp,
-        html: data.HTML
-    }
+export const SendOtpToUser = async (data)=>{
+const mailOptions = {
+    from : Email,
+    to : data.userEmail,
+    subject : 'OTP To Register As Admin',
+    text : data.otp,
+    html : data.HTML
+}
 
-    return new Promise((resolve, reject) => {
-        transport.sendMail(mailOptions, async (error, info) => {
-            if (error) {
-                console.error('OTP Mail Error:', error)
-                reject(error)
-            } else {
-                console.log('OTP Mail Sent Successfully:', info)
-                resolve({ otp: data.otp, info })
-            }
-        })
-    })
+return new Promise((resolve,reject)=>{
+
+
+    transport.sendMail(mailOptions , async (error,info)=>{
+if(error){
+    console.log(error)
+    reject(error)
+}else{
+    console.log(info)
+    resolve({ otp : data.otp , info})
+}
+})
+
+
+})
+
+
+
 }
