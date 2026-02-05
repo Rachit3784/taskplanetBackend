7d11d43ce51e49d7f8a995e5ceef6b30e4172137
Rachit3784 <grachit736@gmail.com>
2026-02-05 16:13:31 +0530

    otp file updation

--- utils/OtpMailer.js
+++ utils/OtpMailer.js
@@ -2,38 +2,40 @@ import nodemailer from 'nodemailer'
 import { Email, Email_Pass } from '../config/ENV_variable.js'
 
 const transport = nodemailer.createTransport({
-    service : 'gmail',
-    auth : {
-        user : Email,
-        pass : Email_Pass
-    }
+    service: 'gmail',
+    auth: {
+        user: Email,
+        pass: Email_Pass
+    },
+    pool: {
+        maxConnections: 1,
+        maxMessages: 100,
+        rateDelta: 2000,
+        rateLimit: 5
+    },
+    socketTimeout: 10000,
+    connectionTimeout: 10000,
+    greetingTimeout: 10000
 });
 
-export const SendOtpToUser = async (data)=>{
-const mailOptions = {
-    from : Email,
-    to : data.userEmail,
-    subject : 'OTP To Register As Admin',
-    text : data.otp,
-    html : data.HTML
-}
+export const SendOtpToUser = async (data) => {
+    const mailOptions = {
+        from: Email,
+        to: data.userEmail,
+        subject: 'OTP To Register As Admin',
+        text: data.otp,
+        html: data.HTML
+    }
 
-return new Promise((resolve,reject)=>{
-
-
-    transport.sendMail(mailOptions , async (error,info)=>{
-if(error){
-    console.log(error)
-    reject(error)
-}else{
-    console.log(info)
-    resolve({ otp : data.otp , info})
-}
-})
-
-
-})
-
-
-
+    return new Promise((resolve, reject) => {
+        transport.sendMail(mailOptions, async (error, info) => {
+            if (error) {
+                console.error('OTP Mail Error:', error)
+                reject(error)
+            } else {
+                console.log('OTP Mail Sent Successfully:', info)
+                resolve({ otp: data.otp, info })
+            }
+        })
+    })
 }
